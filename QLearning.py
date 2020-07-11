3b7f9e56c209db5497a187094a2ddf7cf31f9d05
Maahrii <45564340+Maahrii@users.noreply.github.com>
2020-07-11 17:59:29 +0200

    Initial commit

---
+++ QLearning.py
@@ -0,0 +1,179 @@
+import numpy as np
+import pickle
+
+
+class Game:
+    def __init__(self, player1, player2):
+        self.p1 = player1
+        self.p2 = player2
+        self.board = np.zeros((3, 3))
+        self.end = False
+        self.place_symbol = 1
+        self.win_count = 0
+        self.loss_count = 0
+        self.draw_count = 0
+
+    def get_Hash(self):
+        return str(self.board.flatten())  # gamestate ID
+
+    def available_spots(self):
+        return np.array(np.where(self.board == 0)).T  # list of coordinates that don't have a symbol
+
+    def updateState(self, position):
+        self.board[tuple(position)] = self.place_symbol
+        # switch to another player
+        self.place_symbol = -1 if self.place_symbol == 1 else 1
+
+    def winner(self):
+        # check rows
+        p1rowsum = max(np.sum(self.board, axis=1))
+        p2rowsum = min(np.sum(self.board, axis=1))
+        # check columns
+        p1colsum = max(np.sum(self.board, axis=0))
+        p2colsum = min(np.sum(self.board, axis=0))
+        # check both diagonals
+        diag1sum = self.board.trace()
+        diag2sum = np.rot90(self.board).trace()
+        if (p1rowsum == 3) or (p1colsum == 3) or (diag1sum == 3) or (diag2sum == 3):
+            return 1
+        elif (p2rowsum == -3) or (p2colsum == -3) or (diag1sum == -3) or (diag2sum == -3):
+            return -1
+
+        if len(self.available_spots()) == 0:
+            self.end = True
+            return 0
+        else:
+            return None  # still playing
+
+    def giveReward(self):
+        result = self.winner()
+        # backpropagate reward
+        if result == 1:
+            self.p1.feedReward(2)
+            self.p2.feedReward(-2)
+            self.win_count += 1
+        elif result == -1:
+            self.p1.feedReward(-2)
+            self.p2.feedReward(2)
+            self.loss_count += 1
+        else:
+            self.p1.feedReward(0.5)
+            self.p2.feedReward(0.5)
+            self.draw_count += 1
+
+    def reward(self):
+        self.board = np.zeros((3, 3))
+        self.end = False
+        self.place_symbol = 1
+
+    def reset(self):
+        self.board = np.zeros((3, 3))
+        self.end = False
+        self.place_symbol = 1
+
+    def play(self, rounds):
+        for i in range(rounds):
+            if (i > 0) and (i % (rounds / 10) == 0):
+                print(f'{(i / rounds) * 100}% done')
+                print(f'winrate: {self.win_count / (rounds / 10)}')
+                print(f'lossrate: {self.loss_count / (rounds / 10)}')
+                print(f'drawrate: {self.draw_count / (rounds / 10)}')
+                self.win_count = 0
+                self.loss_count = 0
+                self.draw_count = 0
+            while not self.end:
+                placeable = self.available_spots()
+                p1_action = self.p1.chooseAction(placeable, self.board, self.place_symbol)
+                self.updateState(p1_action)
+                hash = self.get_Hash()
+                self.p1.addState(hash)
+                win = self.winner()
+                if win is not None:  # did player 1 win after their move?
+                    self.giveReward()
+                    self.p1.reset()
+                    self.p2.reset()
+                    self.reset()
+                    break
+                else:
+                    placeable = self.available_spots()
+                    p2_action = self.p1.chooseAction(placeable, self.board, self.place_symbol)
+                    self.updateState(p2_action)
+                    hash = self.get_Hash()
+                    self.p2.addState(hash)
+                    win = self.winner()
+                    if win is not None:  # did player 1 win after their move?
+                        self.giveReward()
+                        self.p1.reset()
+                        self.p2.reset()
+                        self.reset()
+                        break
+
+
+class Agent:
+
+    def __init__(self, name, exp_rate=0.3, lr=0.2, decay_gamma=0.9):
+        self.name = name
+        self.exp_rate = exp_rate
+        self.lr = lr
+        self.decay_gamma = decay_gamma
+        self.states = []
+        self.state_values = {}
+
+
+    def get_Hash(self, board):
+        return str(board.flatten())  # gamestate ID
+
+    def chooseAction(self, positions, current_board, symbol):
+        if np.random.uniform(0, 1) <= self.exp_rate:
+            # take random action
+            index = np.random.choice(len(positions))
+            action = positions[index]
+        else:
+            # take greedy action
+            value_max = -1 * float("inf")
+            for p in positions:
+                next_board = current_board.copy()
+                next_board[p] = symbol
+                next_boardHash = self.get_Hash(next_board)
+                value = 0 if self.state_values.get(next_boardHash) is None else self.state_values.get(next_boardHash)
+                if value >= value_max:
+                    value_max = value
+                    action = p
+        return action
+
+        # append a hash state
+
+    def addState(self, state):
+        self.states.append(state)
+
+    def feedReward(self, reward):
+        for state in reversed(self.states):
+            if self.state_values.get(state) is None:
+                self.state_values[state] = 0
+            self.state_values[state] += self.lr * (self.decay_gamma * reward - self.state_values[state])
+            reward = self.state_values[state]
+
+    def reset(self):
+        self.states = []
+
+    def save_states(self):
+        fw = open('policy_' + str(self.name), 'wb')
+        pickle.dump(self.state_values, fw)
+        fw.close()
+
+    def loadPolicy(self, file):
+        fr = open(file, 'rb')
+        self.state_values = pickle.load(fr)
+        fr.close()
+
+
+if __name__ == "__main__":
+    p1 = Agent("Player1")
+    p2 = Agent("Player2")
+    game = Game(p1, p2)
+
+    print("start training...")
+    game.play(10000)
+    print("done!")
+    p1.save_states()
+
---
+++ Versus PC.py
@@ -0,0 +1,171 @@
+import pygame
+import QLearning
+import numpy as np
+import time
+
+def drawGrid():
+    global screen
+    blockSize = 300  # Set the size of the grid block
+    for x in range(0, width, blockSize):
+        for y in range(0, height, blockSize):
+            rect = pygame.Rect(x, y,
+                               blockSize, blockSize)
+            pygame.draw.rect(screen, BLACK, rect, 1)
+
+def available_spots(board):
+    return np.array(np.where(board == 0)).T  # list of coordinates that don't have a symbol
+
+def get_diag_winner(placed):
+    diag1 = []
+    diag2 = []
+    for block, player in zip(placed.keys(), placed.values()):
+        x, y = block
+        if x + y != 1 or x + y != 3:
+            if x == y:
+                diag1.append(player)
+            if x + y == 2:
+                diag2.append(player)
+    if (len(diag1) == 3 and len(set(diag1)) == 1):
+        return True, diag1[0]
+    elif (len(diag2) == 3 and len(set(diag2)) == 1):
+        return True, diag2[0]
+    return False, 'None'
+
+
+def get_winner(placed):
+    print(get_diag_winner(placed)[0])
+    if not get_diag_winner(placed)[0]:
+        X_count = [[0, 0], [0, 0], [0, 0]]
+        O_count = [[0, 0], [0, 0], [0, 0]]
+        for block, player in zip(placed.keys(), placed.values()):
+            if player == 'X':
+                x, y = block
+                X_count[x][0] += 1
+                X_count[y][1] += 1
+            if player == 'O':
+                x, y = block
+                O_count[x][0] += 1
+                O_count[y][1] += 1
+        if max(sum(X_count, [])) >= 3:
+            return True, 'X'
+        elif max(sum(O_count, [])) >= 3:
+            return True, 'O'
+    else:
+        return get_diag_winner(placed)
+    return False, 'None'
+
+
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+pygame.init()
+
+# Set the width and height of the screen [width, height]
+height = 900
+width = 900
+size = (height, width)
+screen = pygame.display.set_mode(size)
+
+font = pygame.font.Font('freesansbold.ttf', 64)
+
+# create a rectangular object for the
+# text surface object
+
+
+pygame.display.set_caption("Tic Tac Toe")
+
+screen.fill(WHITE)
+drawGrid()
+
+computer = QLearning.Agent("AI")
+computer.loadPolicy("policy_Player1")
+computer_board = np.zeros((3,3))
+X = pygame.image.load('X.png')
+O = pygame.image.load('O.png')
+X = pygame.transform.smoothscale(X, (300, 300))
+O = pygame.transform.smoothscale(O, (300, 300))
+placed = {}
+turn = 0
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+done = False
+won = False
+run = True
+
+# -------- Main Program Loop -----------
+while run:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if turn % 2 == 0:
+            placeable = available_spots(computer_board)
+            p1_action = computer.chooseAction(placeable, computer_board, 1)
+            x,y = tuple(p1_action)
+            computer_board[(x,y)] = 1
+            placed[(x,y)] = 'X'
+            screen.blit(X, (x * 300, y * 300))
+            turn += 1
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x, y = event.pos
+            x, y = x // 300, y // 300
+            if turn % 2 != 0:
+                if (x, y) not in placed:
+                    computer_board[(x, y)] = -1
+                    placed[(x, y)] = 'O'
+                    screen.blit(O, (x * 300, y * 300))
+                    turn += 1
+            if won:
+                won = False
+                screen.fill(WHITE)
+                drawGrid()
+                placed = {}
+                computer_board = np.zeros((3,3))
+                turn = 0
+            if done:
+                done = False
+                screen.fill(WHITE)
+                drawGrid()
+                placed = {}
+                computer_board = np.zeros((3, 3))
+                turn = 0
+    if len(placed.keys()) > 3:
+        won, winner = get_winner(placed)
+    if won:
+        vict = font.render(f'Game Over: {winner} won!', True, WHITE, BLUE)
+        textRect = vict.get_rect()
+        textRect.center = (width // 2, height // 2)
+        screen.fill(WHITE)
+        screen.blit(vict, textRect)
+
+    if len(placed.keys()) == 9 and not won:
+        done = True
+        draw = font.render('Game Over: Draw', True, RED, BLUE)
+        textRect = draw.get_rect()
+        textRect.center = (width // 2, height // 2)
+        screen.fill(WHITE)
+        screen.blit(draw, textRect)
+        # --- Game logic should go here
+
+    # --- Screen-clearing code goes here
+
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+
+    # --- Drawing code should go here
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(10)
+
+# Close the window and quit.
+pygame.quit()
